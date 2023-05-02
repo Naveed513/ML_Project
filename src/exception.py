7379107c67c7f23c6f9da717007a13dd17c19ac3
Naveed <sknaveed513@gmail.com>
2023-05-02 16:18:46 +0530

    exception.py with logger method enabled

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 
 def error_message_detail(error, error_detail:sys):
@@ -17,6 +17,7 @@ class CustomException(Exception):
         self.error_message = error_message_detail(error_message, error_detail = error_detail)
 
     def __str__(self):
+        logging.info(self.error_message)
         return self.error_message
     
         
